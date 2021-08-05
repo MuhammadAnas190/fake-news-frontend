657cbda5bce689e87a94940d9657174ed699d67c
MuhammadAnas190 <mohammad.anas190@gmail.com>
2021-08-05 23:07:47 +0500

    feat: add antd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { MainView } from "./view/index";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <MainView />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/view/index.jsx
@@ -0,0 +1,7 @@
+import { Button } from "antd";
+
+export const MainView = (props) => (
+  <>
+    <Button>Im a button</Button>
+  </>
+);
