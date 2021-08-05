f64b3a32a365d33afff58114d7aeb55065577c93
MuhammadAnas190 <mohammad.anas190@gmail.com>
2021-08-05 23:15:14 +0500

    feat: add css and remove logo svg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import { MainView } from "./view/index";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <MainView />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MainView />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
