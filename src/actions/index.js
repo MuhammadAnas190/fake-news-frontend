2e1f16f16feb769abc2c53e97ea7684163b6c724
MuhammadAnas190 <mohammad.anas190@gmail.com>
2021-08-08 21:12:59 +0500

    feat: set axios and set default config

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,19 @@
 /**
- * Here we will be handling our actions related to API
+ * Here we will be dispatching API actions
  */
+import axios from "axios";
+
+// We need to set default config for axios
+axios.defaults.baseURL = "https://api.example.com";
+axios.defaults.timeout = 3000;
+axios.defaults.headers.post["Content-Type"] =
+  "application/x-www-form-urlencoded";
+// We will be using async function for dispatching network requests
+export async function getUser() {
+  try {
+    const response = await axios.get("/user?ID=12345");
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
