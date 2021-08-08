a40182406e00bb83e2ec6ea90657fed50a2ba248
Mohammad Anas <mohammad.anas190@gmail.com>
2021-08-08 21:16:00 +0500

    merge - feat: set axios and set default config (#1)

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,13 @@
 import { Button } from "antd";
+import PropTypes from "prop-types";
 import "./style.css";
 
 export const HXButton = ({ className = "", ...props }) => {
   const cls = `${className} hx-button`;
   return <Button {...props} className={cls} />;
+};
+
+// Setting prop types
+HXButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
 };
