1f41ca673c36367667c77fdbd9ac120fa2c437cc
subhan <subhan97ahmed@gmail.com>
2021-08-16 02:22:48 +0500

    connected frontend with backend api for classification and renamed actions folder to utils

---
+++ src/utils/index.js
@@ -0,0 +1,26 @@
+/**
+ * Here we will be dispatching API actions
+ */
+import { message } from "antd";
+import axios from "axios";
+
+// We need to set default config for axios
+axios.defaults.baseURL = "https://fake-news-detector-backend.herokuapp.com";
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
+axios.defaults.headers.post['accept'] = "application/json"
+axios.defaults.headers.post["Content-Type"] =
+  "application/json";
+// We will be using async function for dispatching network requests
+export async function fetchNewsResult(newsText) {
+  try {
+    const response = await axios.post('/detect', {
+      text: newsText
+    });
+    console.log(response.data);
+    message.success('Calculated Successfully')
+    const result = await response.data
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
--- src/view/Main.jsx
+++ src/view/Main.jsx
@@ -1,7 +1,7 @@
-import { Layout, Row, Col, Typography } from "antd";
-
-import { HXImage } from "components/index";
-
+import { useState } from "react";
+import { Layout, Row, Col, Typography, message } from "antd";
+import { HXImage, HXButton, HXTextArea } from "components/index";
+import { fetchNewsResult } from "utils";
 import Logo from "assets/images/logo.png";
 import FakeNews from "assets/images/fake-news.jpg";
 
@@ -11,7 +11,9 @@ const { Header, Content } = Layout;
 const { Title, Text } = Typography;
 
 /**
- * @author MuhammadAnas190
+ * @authors
+ * MuhammadAnas190
+ * Subhan Ahmed
  * We will be setting a basic layout for the web.
  * Also we should be calling our actions from here
  * and pass down the values through props (single responsibility)
@@ -20,6 +22,38 @@ const { Title, Text } = Typography;
  * .main-view
  */
 const MainView = (props) => {
+  const [newsText, setnewsText] = useState("");
+  const [result, setresult] = useState("");
+
+  const OnChangeHXTextArea = (e) => {
+    setnewsText(e.target.value);
+    console.log(newsText);
+  };
+  const onClickSubmit = async () => {
+    // removing links and special symbols from th news text
+    let temp = newsText.toLowerCase();
+    // temp = temp.replace("/[https?://S+|www.S+]/g", "");
+    temp = temp.replace(/[\\W]/gm, " ");
+    // temp = temp.replace("/[\n]/g", "");
+    // temp = temp.replace(/[+]/g, " ");
+    // temp = temp.replace(/[^]/g, "");
+    // temp = temp.replace(/[$]/g, "");
+    // temp =temp.replace(/[^\x20-\x7E]/gmi, "")
+    temp = temp.replace(/(\r\n|\n|\r)/gm, " ").trim();
+    temp = temp.replace(/[â€™]/g, "");
+    temp = temp.replace(/[â€œ]/g, "");
+    temp = temp.replace(/[]/g, "");
+    // temp = temp.replace(/[^a-zA-Z0-9]/g, '');
+
+    console.log(temp);
+    setnewsText(temp);
+    if (newsText) {
+      message.loading("Calculating news");
+      const tempr = await fetchNewsResult(newsText);
+      setresult(tempr);
+      console.log(result);
+    }
+  };
   return (
     <Layout className="main-view">
       <Header>
@@ -43,6 +77,34 @@ const MainView = (props) => {
                   is the correct article. We are using Tensorflow model through
                   which we will be finding out the fake news.
                 </Text>
+                <Row>
+                  <HXTextArea
+                    placeholder="News here"
+                    onChange={OnChangeHXTextArea}
+                    size="large"
+                    style={{ height: "150px" }}
+                  />
+                </Row>
+                <HXButton
+                  onClick={onClickSubmit}
+                  style={{ float: "right", marginTop: "10px" }}
+                >
+                  Analyze
+                </HXButton>
+                <Row>
+                  <Col>
+                    {result.prediction === "Real" ? (
+                      <p style={{ color: "green", marginTop: "10px" }}>
+                        Its Real
+                      </p>
+                    ) : null}
+                    {result.prediction === "Fake" ? (
+                      <p style={{ color: "red", marginTop: "10px" }}>
+                        Its Fake
+                      </p>
+                    ) : null}
+                  </Col>
+                </Row>
               </Col>
               <Col lg={12}>
                 <Row justify="center">
